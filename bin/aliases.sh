550250aea529ad37c340fa1a7cf6417ef0c38009
Brian Kohles <briankohles@users.noreply.github.com>
2021-12-26 23:27:21 -0700

    adding aliases & djifix files

---
+++ bin/aliases.sh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+
+# alias to ssh to pi
+alias sshpi='ssh pi@129.168.2.135'
+
+# pytest
+alias pytest='python3 /Volumes/Data_2TB/Users/briankohles/Library/Python/2.7/lib/python/site-packages/pytest.py'
